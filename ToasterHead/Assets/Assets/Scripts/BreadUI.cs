c5ef27bb5ce8092e555c6e1cf785e21ab45864d1
Martin Briand <martinbriands@gmail.com>
2017-10-12 14:43:38 -0400

    toaster head added

---
+++ ToasterHead/Assets/Assets/Scripts/BreadUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadUI : MonoBehaviour {
+	float i = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		i++;
+		this.transform.localRotation = Quaternion.Euler( new Vector3 (-41 , i, 0));
+	}
+}
---
+++ ToasterHead/Assets/Assets/Scripts/HeadController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour {
+
+	public GameObject sphere;
+	public LineRenderer line;
+	public Camera VR;
+	// Use this for initialization
+	void Start () {
+		//line = gameObject.GetComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.LookAt (sphere.transform);
+
+		transform.rotation = VR.gameObject.transform.rotation;
+
+		Vector3 forward = transform.TransformDirection (Vector3.forward) * 10;
+		Debug.DrawRay (transform.position, forward, Color.red);
+
+		//line.SetPosition (0, transform.position);
+		//line.SetPosition (1, sphere.transform.position);
+
+	}
+}
